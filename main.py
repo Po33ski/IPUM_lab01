e0321ba808bb9f9696f3f93625b3953a1a932dd1
Po33ski <jaroslaw.popardowski@wp.pl>
2025-04-29 19:24:04 +0200

    encrypted

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from ipum-lab01!")
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+from pathlib import Path
+
+
+def export_envs(environment: str = "dev") -> None:
+    if environment == "dev":
+        load_dotenv(dotenv_path=Path("env.dev"))
+    elif environment == "prod":
+        load_dotenv(dotenv_path=Path("env.prod"))
+    elif environment == "test":
+        load_dotenv(dotenv_path=Path("env.test"))
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
---
+++ settings.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+from pydantic import validator, ValidationError
+
+
+class Settings(BaseSettings):
+    ENVIRONMENT: str
+    APP_NAME: str
+
+    @validator("ENVIRONMENT")
+    def validate_environment(cls, value):
+        env_values = ["dev", "test", "prod"]
+        if value in env_values:
+            return value
+        else:
+            raise ValidationError
