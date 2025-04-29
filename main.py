34ef6fa0b49f210b20752701bab63c43073f0b2e
Po33ski <jaroslaw.popardowski@wp.pl>
2025-04-29 20:50:32 +0200

    settings and main motified

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 from dotenv import load_dotenv
 from settings import Settings
 from pathlib import Path
+import yaml
+import os
 
 
 def export_envs(environment: str = "dev") -> None:
@@ -11,6 +13,13 @@ def export_envs(environment: str = "dev") -> None:
         load_dotenv(dotenv_path=Path(".env.prod"))
     elif environment == "test":
         load_dotenv(dotenv_path=Path(".env.test"))
+
+
+def load_secrets_from_yaml(path="secrets.yaml"):
+    with open(path, "r") as f:
+        secrets = yaml.safe_load(f)
+        for key, value in secrets.items():
+            os.environ[key] = str(value)
 
 
 if __name__ == "__main__":
@@ -27,7 +36,10 @@ if __name__ == "__main__":
 
     export_envs(args.environment)
 
+    load_secrets_from_yaml()
+
     settings = Settings()
 
     print("APP_NAME: ", settings.APP_NAME)
     print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("SECRET_API_KEY:", settings.SECRET_API_KEY)
--- settings.py
+++ settings.py
@@ -5,9 +5,10 @@ from pydantic import field_validator, ValidationError
 class Settings(BaseSettings):
     ENVIRONMENT: str
     APP_NAME: str
+    SECRET_API_KEY: str
 
     @field_validator("ENVIRONMENT")
-    def validate_environment(cls, value):
+    def validate_environment(value):
         env_values = ["dev", "test", "prod"]
         if value in env_values:
             return value
